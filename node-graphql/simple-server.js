b3cfc6cb5c044ae554b23b09c39bc73e9bcf0972
sergio <sergioverde90@gmail.com>
2018-01-26 13:17:16 +0100

    refactor

--- node-graphql/simple-server.js
+++ node-graphql/simple-server.js
@@ -1,20 +1,44 @@
 const { graphql, buildSchema } = require('graphql');
+const fs = require('fs');
+const users = JSON.parse(fs.readFileSync('./users.json', 'utf8'));
 
+// from: https://wehavefaces.net/graphql-shorthand-notation-cheatsheet-17cd715861b6#.9oztv0a7n
 const SCHEMA = buildSchema(`
-    type Query {
-        idUser : String,
+
+    type User {
+        id : String,
         name : String
-    }`);
-
-function api() { return graphql(SCHEMA, `{ idUser }` , lookupUser()); }
-
-function lookupUser() {
-    // some lookup ...
-    return {
-        idUser : () => "1",
-        name : () => "Sergio"
     }
+
+    type Query {
+        users : [User]!
+        user(id : String) : User!
+    }
+
+`);
+
+// The root provides a resolver function for each API endpoint
+const ENDPOINT = {
+    // the 'users' endpoint returns all users
+    'users': () => users,
+    'user' : (args) => {
+        return users.filter((user) => {
+            return user.id === args['id']
+        })[0];
+    }
+};
+
+function all() { 
+    return graphql(SCHEMA, `{ users { id } }`, ENDPOINT); 
 }
 
-// example query
-api().then(r => console.log(r));
+function single(id) {
+    return graphql(SCHEMA, `{ user(id: "${id}") { id } }`, ENDPOINT); 
+}
+
+// test api
+all().then(r => console.log(r.data.users));
+single("1000").then(r => console.log(r.data.user));
+single("541")
+    .then(r => console.log(r.data.user))
+    .catch(err => console.error("user not found :("));
