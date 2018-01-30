2fa3aa3588b4137de586ec1e8d5208f80fc0cc46
sergio <sergioverde90@gmail.com>
2018-01-30 13:24:55 +0100

    refactor

--- node-websocket/main.js
+++ node-websocket/main.js
@@ -1,3 +1,4 @@
+
 const Connection = function() {
     let socket;
     return {
@@ -5,6 +6,9 @@ const Connection = function() {
             this.socket = new WebSocket(url);
             this.socket.addEventListener('open', (event) => { console.log('connection established!'); });
             this.socket.onerror = (err) =>  { console.error("connection error => ", err); };
+        },
+        disconnect : () => {
+            this.socket.close();
         },
         send : (data) => {
             this.socket.send(data);
@@ -15,17 +19,32 @@ const Connection = function() {
         on : (action, callback) => {
             this.socket.addEventListener(action, callback);
         },
-        close : () => {
-            this.socket.close();
+        isConnected : () => {
+            return this.socket && this.socket.readyState === 1;
         }
     }
 }();
+
+function toogleConnection() {
+    if(!Connection.isConnected()) {
+        connect();
+    } else {
+        disconnect();
+    }
+}
 
 function connect() {
     Connection.connect('ws://192.168.1.123:8080');
     Connection.on('message', (event) => { document.getElementById('shared').value = event.data; })
     document.getElementById('shared').disabled = false;
-    document.getElementById('btn-connect').disabled = true;
+    document.getElementById('btn-connect').innerHTML = 'Disconnect';
+}
+
+function disconnect() {
+    Connection.disconnect();
+    document.getElementById('shared').disabled = true;
+    document.getElementById('shared').value = '';
+    document.getElementById('btn-connect').innerHTML = 'Connect';
 }
 
 function modify(event) {
